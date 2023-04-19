af05a997e2a9ab068a1e931f22a991fc4d906c60
SenAzureDemo1 <senazuredemo1@gmail.com>
2023-04-19 18:14:47 -0400

    add prime number

--- CodingChallenges/PrimeNumber.cs
+++ CodingChallenges/PrimeNumber.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,60 @@ namespace CodingChallenges
 {
     public class PrimeNumber
     {
+        private readonly int sequenceCount;
+        private List<int> sequence = new List<int>();
+
+        public PrimeNumber(int n)
+        {
+            this.sequenceCount = n;
+        }
+
+        public List<int> GetSequence()
+        {
+            CalculateSequence();
+            return sequence;
+        }
+
+        private void CalculateSequence()
+        {
+            int number = 0;
+            while (sequence.Count < sequenceCount)
+            {
+                //call a method to check if the number is prime number
+                //if prime number
+                //  then add that number to squencelist
+                //  else increment the number and continue the process
+
+                var isPrimeNumber = CheckPrimeNumber(number);
+                if(isPrimeNumber)
+                {
+                    sequence.Add(number);
+                }
+                number++;
+            }
+        }
+
+        private bool CheckPrimeNumber(int number)
+        {
+            var isPrime = false;
+            var remainderExecuted = false;
+            if(number > 1)
+            {
+                isPrime = true;
+                for (int i = 1; i <= number; i++)
+                {
+                    if(i != 1 && i != number)
+                    {
+                        var remainder = number % i;
+                        if (remainder == 0) isPrime = false;
+                        remainderExecuted = true;
+                    }
+                }
+
+                if (!remainderExecuted) isPrime = true;
+            }
+            return isPrime;
+        }
+
     }
 }
---
+++ CodingChallengesTest/PrimeNumberTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengesTest
+{
+    [TestFixture]
+    public class PrimeNumberTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /* Requirement
+         * a whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1 
+         * Examples 2, 3, 5, 7, 11
+         * 
+         * f(1) = 2
+         * f(2) = 2,3
+         * f(3) = 2, 3, 5
+         * f(4) = 2, 3, 5, 7
+         * f(5) = 2, 3, 5, 7, 11
+         * f(100) = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+         */
+
+        [Test]
+        public void TestPrimeNumberWithN1()
+        {
+            var primeNumber = new PrimeNumber(1);
+            var list = primeNumber.GetSequence();
+
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual(2, list[0]);
+        }
+
+        [Test]
+        public void TestPrimeNumberWithN2()
+        {
+            var primeNumber = new PrimeNumber(2);
+            var list = primeNumber.GetSequence();
+
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(2, list[0]);
+            Assert.AreEqual(3, list[1]);
+        }
+
+        [Test]
+        public void TestPrimeNumberWithN3()
+        {
+            var primeNumber = new PrimeNumber(3);
+            var list = primeNumber.GetSequence();
+
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual(2, list[0]);
+            Assert.AreEqual(3, list[1]);
+            Assert.AreEqual(5, list[2]);
+        }
+
+        [Test]
+        [TestCase(5, 11)]
+        [TestCase(6, 13)]
+        [TestCase(7, 17)]
+        [TestCase(8, 19)]
+        public void TestPrimeNumberWithN(int n, int expected)
+        {
+            var primeNumber = new PrimeNumber(n);
+            var list = primeNumber.GetSequence();
+
+            Assert.AreEqual(n, list.Count);
+            Assert.AreEqual(expected, list[n-1]);
+
+        }
+
+    }
+}
