7d44d8a20e488816d46e9eeda007acbed1c56d30
SenAzureDemo1 <senazuredemo1@gmail.com>
2023-04-19 00:06:22 -0400

    added fibonacci

---
+++ CodingChallenges/Fibonacci.cs
@@ -0,0 +1,68 @@
+﻿namespace CodingChallenges
+{
+    public class Fibonacci
+    {
+        private int sequenceCount { get; set; }
+        private List<int> sequence = new List<int>();
+        public Fibonacci(int n)
+        {
+            this.sequenceCount = n;
+        }
+
+        public List<int> GetSequence()
+        {
+            //0, 1, 1, 2
+            var i = 0;
+            while(i<= sequenceCount)
+            {
+                sequence.Add(calculateSequence(i));
+                i++;
+            }
+          
+            return sequence;
+        }
+
+        private int calculateSequence(int index)
+        {
+            int val = 0;
+
+            if (index == 0)
+            {
+                val = 0;
+            }
+            if (index == 1)
+            {
+                val = 1;
+            }
+            if (index >=2)
+            {
+                var item = sequence[index - 2] + sequence[index - 1];
+                val = item;
+            }
+            return val;
+        }
+
+        //private void calculateSequence()
+        //{
+
+        //    if(sequenceCount == 0)
+        //    {
+        //        sequence.Add(0);
+        //    }
+        //    if (sequenceCount == 1)
+        //    {
+        //        sequence.Add(0);
+        //        sequence.Add(1);
+        //    }
+        //    if (sequenceCount == 2)
+        //    {
+        //        sequence.Add(0);
+        //        sequence.Add(1);
+
+        //        var item3 = sequence[sequenceCount - 2] + sequence[sequenceCount - 1];
+        //        sequence.Add(item3);
+        //    }
+
+        //}
+    }
+}
---
+++ CodingChallengesTest/FibonacciTests.cs
@@ -0,0 +1,81 @@
+namespace CodingChallengesTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestFibonacciWith0()
+        {
+            var fibonacci = new Fibonacci(0);
+            var sequence = fibonacci.GetSequence(); //0
+
+            Assert.AreEqual(1, sequence.Count);
+            Assert.AreEqual(0,sequence[0]);
+        }
+
+        [Test]
+        public void TestFibonacciWith1()
+        {
+            var fibonacci = new Fibonacci(1);
+            var sequence = fibonacci.GetSequence(); //0,1
+
+            Assert.AreEqual(2, sequence.Count);
+            Assert.AreEqual(0, sequence[0]);
+            Assert.AreEqual(1, sequence[1]);
+        }
+
+        [Test]
+        public void TestFibonacciWith2()
+        {
+            var fibonacci = new Fibonacci(2);
+            var sequence = fibonacci.GetSequence(); //0,1,1
+
+            Assert.AreEqual(3, sequence.Count);
+            Assert.AreEqual(0, sequence[0]);
+            Assert.AreEqual(1, sequence[1]);
+            Assert.AreEqual(1, sequence[2]);
+        }
+
+        [Test]
+        public void TestFibonacciWith3()
+        {
+            var fibonacci = new Fibonacci(3);
+            var sequence = fibonacci.GetSequence(); //0,1,1,2
+
+            Assert.AreEqual(4, sequence.Count);
+            Assert.AreEqual(0, sequence[0]);
+            Assert.AreEqual(1, sequence[1]);
+            Assert.AreEqual(1, sequence[2]);
+            Assert.AreEqual(2, sequence[3]);
+        }
+
+        [Test]
+        public void TestFibonacciWith4()
+        {
+            var fibonacci = new Fibonacci(4);
+            var sequence = fibonacci.GetSequence(); //0,1,1,2,3
+
+            Assert.AreEqual(5,sequence.Count);
+            Assert.AreEqual(0, sequence[0]);
+            Assert.AreEqual(1, sequence[1]);
+            Assert.AreEqual(1, sequence[2]);
+            Assert.AreEqual(2, sequence[3]);
+            Assert.AreEqual(3, sequence[4]);
+        }
+
+        [Test]
+        public void TestFibonacciWith7()
+        {
+            var fibonacci = new Fibonacci(7);
+            var sequence = fibonacci.GetSequence(); //0,1,1,2,3,5,8,13
+
+            Assert.AreEqual(8, sequence.Count);
+            Assert.AreEqual(13, sequence[7]);
+        }
+    }
+}
