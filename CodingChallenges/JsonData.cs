0792e6640ee53a250acd1debc2988391ebfd4f1f
Senthil Elayappan <elayas1@sigi.us.selective.com>
2023-04-20 15:05:58 -0400

    added json code logic

--- CodingChallenges/JsonData.cs
+++ CodingChallenges/JsonData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace CodingChallenges
 {
-    internal class JsonData
+    public class JsonData
     {
+
+        public class WeatherForecast
+        {
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureCelsius { get; set; }
+            public string? Summary { get; set; }
+        }
+
+
+        public string GetWeatherJson()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+            var json = JsonConvert.SerializeObject(weatherForecast);
+            return json;
+        }
+
+        public async Task<string> GetWeatherJsonFromService(string url)
+        {
+            var json = string.Empty;
+            try
+            {
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(url);
+
+                response.EnsureSuccessStatusCode();
+                json = await response.Content.ReadAsStringAsync();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return json;
+        }
+
     }
 }
---
+++ CodingChallengesTest/JasonDataTests.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengesTest
+{
+    [TestFixture]
+    public class JasonDataTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        /* Requirements
+         * make a json webserivce call
+         * do the manipulation in the json response
+         * 
+         */
+
+        [Test]
+        public void TestJsonWebServiceCall()
+        {
+            string url = "https://dummyjson.com/products";
+            var json = new JsonData().GetWeatherJsonFromService(url);
+
+        }
+
+        [Test]
+        public void TestJsonManipulation()
+        {
+            var json = new JsonData().GetWeatherJson();
+
+            //By Matching the Property Name
+            var weather = JsonConvert.DeserializeObject<WeatherForecast>(json);
+            Assert.That(weather,Is.InstanceOf<WeatherForecast>());
+            Assert.True(!String.IsNullOrEmpty(weather.Summary));
+
+            //If Property names are not mached, then use the DTO
+            var weatherDto = JsonConvert.DeserializeObject<WeatherForecastDTO>(json);
+            Assert.That(weatherDto, Is.InstanceOf<WeatherForecastDTO>());
+            Assert.True(!String.IsNullOrEmpty(weatherDto.Details));
+
+            //Covert DTO object to json
+            var json1 = JsonConvert.SerializeObject(weather);
+            Assert.IsNotEmpty(json1);
+        }
+
+        public class WeatherForecast
+        {
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureCelsius { get; set; }
+            public string? Summary { get; set; }
+        }
+
+        public class WeatherForecastDTO
+        {
+            [JsonProperty("Date")]
+            public string? CurrentDate { get; set; }
+            [JsonProperty("TemperatureCelsius")]
+            public int Temperature { get; set; }
+            [JsonProperty("Summary")]
+            public string? Details { get; set; }
+        }
+    }
+}
