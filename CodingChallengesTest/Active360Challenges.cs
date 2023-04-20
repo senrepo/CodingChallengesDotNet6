5ea616b60343376630e9463aa30831c357376210
SenAzureDemo1 <senazuredemo1@gmail.com>
2023-04-20 15:11:29 -0400

    added Active360 challenges

---
+++ CodingChallengesTest/Active360Challenges.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengesTest
+{
+    public class Active360Challenges
+    {
+        // Initialize a Dictionary of key{long} and value{objects}, convert it to a list based on any condition and thenfilter the list by multiple properties and then sort them
+        // Use arrow inline function 1. takes in 2 number and return the sum, 2. takes the sum and print it to the console= 
+        // Recursive function to find a factorial of a number  
+
+
+    }
+}
