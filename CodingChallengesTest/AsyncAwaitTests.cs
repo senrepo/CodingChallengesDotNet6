f5e4b34e407757607bc30566159e0caaaf4dbd56
SenAzureDemo1 <senazuredemo1@gmail.com>
2023-04-22 00:09:31 -0400

    async and await samples added

--- CodingChallengesTest/AsyncAwaitTests.cs
+++ CodingChallengesTest/AsyncAwaitTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -61,45 +63,128 @@ namespace CodingChallengesTest
             Console.WriteLine("Working...");
         }
 
+        [Test]
         public async Task TaskRun()
         {
             //create 20 random rumbers from 1-20 and calculate the factorial
             //CPU bound operation
 
-            Func<int, int> factorial = delegate (int n)
+            var factorialList = new List<Factorial>();
+            var random = new Random();
+            int number = 0;
+            for(int i = 1; i <= 10; i++)
             {
-                if (n ==1) return 1;
-                else return n * factorial(n-1);
+                number = random.Next(1, 10);
+                factorialList.Add(new Factorial { Number = number});
+            }
+
+            var tasks = new List<Task>();
+            foreach(var item in factorialList)
+            {
+                var task = Task.Factory.StartNew((obj) =>
+                {
+                    var factorial = obj as Factorial;
+                    factorial!.Value = RecursiveFactorial(factorial.Number);
+
+                }, item);
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            foreach(var item in factorialList)
+            {
+                Console.WriteLine($"{item.Number} factorial value is {item.Value}");
+            }
+        }
+
+        [Test]
+        public async Task TaskRun1()
+        {
+            //create 20 random rumbers from 1-20 and calculate the factorial
+            //CPU bound operation
+
+            var factorialList = new List<KeyValuePair<int, Task<int>>>();
+            var random = new Random();
+            int number = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                number = random.Next(1, 10);
+                var task = Task<int>.Factory.StartNew((obj) =>
+                {
+                    var factorialNumber = Convert.ToInt32(obj);
+
+                    return RecursiveFactorial(factorialNumber);
+
+                },number);
+                factorialList.Add(new KeyValuePair<int, Task<int>>(number, task));
+            }
+
+            foreach (var item in factorialList)
+            {
+                Console.WriteLine($"{item.Key} factorial value is {item.Value.Result}");
+            }
+        }
+
+        private int RecursiveFactorial(int n)
+        {
+            if (n == 1) return 1;
+            else return n * RecursiveFactorial(n - 1);
+        }
+
+        private class Factorial
+        {
+            public int Number { get; set; }
+            public int Value { get; set; }
+
+        }
+
+        [Test]
+        public async Task TaskParallell()
+        {
+            /* Concurrency 
+             *  Consider two tasks, T1 and T2, that have to be executed by an application. 
+             *  if one is in an execution state while the other is waiting for its turn.
+             *  As a result, one of the tasks completes ahead of the other.
+             */
+
+
+            /* Parallelism 
+             *  Consider two tasks, T1 and T2, that have to be executed by an application. 
+             *  the two tasks are in parallel execution if both execute simultaneously. 
+             *  To achieve task parallelism, the program must run on a CPU with multiple cores.
+             */
+
+            Func<int, bool> IsPrime = delegate (int integer)
+            {
+                if (integer <= 1) return false;
+                if (integer == 2) return true;
+                var limit = Math.Ceiling(Math.Sqrt(integer));
+                for (int i = 2; i <= limit; ++i)
+                    if (integer % i == 0)
+                        return false;
+                return true;
             };
 
-            var factorialDict = new Dictionary<int, int>();
-            var random = new Random();
-            for(int i = 1; i <= 20; i++)
+            var list = new List<int>();
+            for(var i = 1; i < 100; ++i)
             {
-                var key = random.Next(1, 20);
-                factorialDict.Add(key, 0);
+                list.Add(i);
             }
 
-            var jobs = new List<Job>();
-            foreach(var key in factorialDict.Keys)
+            var concurrentList = new ConcurrentBag<int>();
+            Parallel.ForEach(list, number =>
             {
-                var job = new Job()
-                {
-                    Key = key,
-                    Task = new Task(async () =>
-                    {
+                if (IsPrime(number)) concurrentList.Add(number);
+            });
 
-                    })
-                };
+            Console.WriteLine("Prime number list between 1 to 100");
+            foreach(var item in concurrentList)
+            {
+                Console.WriteLine(item);
             }
-
-
         }
 
-        public class Job
-        {
-            public int Key = 0;
-            public Task Task { get; set; }
-        }
+
     }
 }
