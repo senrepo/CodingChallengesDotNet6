a6e1d6bc49e408bcce83572026fe8a0e4b4cbc90
senrepo <senthil.elayappan@yahoo.com>
2023-05-11 08:18:11 -0400

    other pattern placeholders added

--- CodingChallengesTest/DesignPatternChallenge.cs
+++ CodingChallengesTest/DesignPatternChallenge.cs
@@ -13,12 +13,39 @@ namespace CodingChallengesTest
         [Test]
         public void TestBuilderPattern()
         {
+            //Builder pattern, Template pattern, Factory pattern
             var cashier = new Cashier();
             var vegMeal = cashier.Order("VeggieBurgerMeal");
             var nonVegMeal = cashier.Order("ChickenBurgerMeal");
 
             var vegmealPlate = $"MainItem:{vegMeal.MainItem}, SideItem:{vegMeal.SideItem}, Drink: {vegMeal.Drink}";
             var nonVegmealPlate = $"MainItem:{nonVegMeal.MainItem}, SideItem:{nonVegMeal.SideItem}, Drink: {nonVegMeal.Drink}";
+        }
+
+        public void TestStrategyPattern()
+        {
+            //Strategy pattern - handle the variable behaviours
+
+        }
+
+        public void TestObserverPattern()
+        {
+
+        }
+
+        public void TestSingletonPattern()
+        {
+
+        }
+
+        public void TestFacadePattern()
+        {
+
+        }
+
+        public void TestRepositoryPattern()
+        {
+
         }
 
     }
